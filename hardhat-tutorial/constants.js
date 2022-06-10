881f31df47f274dc9dcf16838d4fceae381d6bf8
Mungunshagai <mungunshagai@coinhub.mn>
2022-06-10 17:45:40 +0800

    first commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xe841424fB1a3ABd1f0930F4F5Cf7C81e5b45b7d7";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
